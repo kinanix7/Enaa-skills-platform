f0ade09f2f6ae918594fcd87433e1d32813d0ae1
kinanix7 <kinanix7279x@gmail.com>
2025-06-30 11:35:16 +0100

    Initialisation du projet Spring Boot ENAA Skills

---
+++ src/main/java/com/example/enaaskillsplatform/EnaaSkillsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.enaaskillsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnaaSkillsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnaaSkillsPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enaaskillsplatform/EnaaSkillsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enaaskillsplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnaaSkillsPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
